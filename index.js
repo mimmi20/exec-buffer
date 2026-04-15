42720643190d2f438f4b19f584b9d474b5a9e3a5
Thomas Müller <mimmi20@live.de>
2026-04-15 07:02:12 +0200

    fix isues

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 import { writeFile, readFile } from 'node:fs/promises';
 import { execa } from 'execa';
-import { rimraf } from 'rimraf'
+import { rimraf } from 'rimraf';
 import tempfile from 'tempfile';
 
 export const input = Symbol('inputPath');
 export const output = Symbol('outputPath');
 
-const pFinally = async function(
-  promise,
-  onFinally = (() => {})
-) {
+const pFinally = async function (promise, onFinally = () => {}) {
   let value;
   try {
     value = await promise;
@@ -22,36 +19,31 @@ const pFinally = async function(
   return value;
 };
 
-const func = async opts => {
-	opts = Object.assign({}, opts);
+const func = async (opts) => {
+  opts = { ...opts };
 
-	if (!Buffer.isBuffer(opts.input)) {
-		return Promise.reject(new Error('Input is required'));
-	}
+  if (!Buffer.isBuffer(opts.input)) {
+    return Promise.reject(new Error('Input is required'));
+  }
 
-	if (typeof opts.bin !== 'string') {
-		return Promise.reject(new Error('Binary is required'));
-	}
+  if (typeof opts.bin !== 'string') {
+    return Promise.reject(new Error('Binary is required'));
+  }
 
-	if (!Array.isArray(opts.args)) {
-		return Promise.reject(new Error('Arguments are required'));
-	}
+  if (!Array.isArray(opts.args)) {
+    return Promise.reject(new Error('Arguments are required'));
+  }
 
-	const inputPath = opts.inputPath || tempfile();
-	const outputPath = opts.outputPath || tempfile();
+  const inputPath = opts.inputPath || tempfile();
+  const outputPath = opts.outputPath || tempfile();
 
-	opts.args = opts.args.map(function (x) {
-		return x === input ? inputPath : x === output ? outputPath : x;
-	});
+  opts.args = opts.args.map((x) => (x === input ? inputPath : x === output ? outputPath : x));
 
-	const promise = writeFile(inputPath, opts.input)
-		.then(() => execa(opts.bin, opts.args))
-		.then(() => readFile(outputPath));
+  const promise = writeFile(inputPath, opts.input)
+    .then(() => execa(opts.bin, opts.args))
+    .then(() => readFile(outputPath));
 
-	return pFinally(promise, () => Promise.all([
-		rimraf(inputPath),
-		rimraf(outputPath)
-	]));
+  return pFinally(promise, () => Promise.all([rimraf(inputPath), rimraf(outputPath)]));
 };
 
 export default func;
--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import { readFile } from 'node:fs/promises';
 import path from 'node:path';
 import gifsicle from 'gifsicle';
 import isGif from 'is-gif';
-import { pathExists } from 'path-exists'
+import { pathExists } from 'path-exists';
 import test from 'ava';
 import m, { input, output } from './index.js';
 import { fileURLToPath } from 'node:url';
@@ -10,45 +10,45 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-test('set temporary directories', async t => {
-	t.truthy(input);
-	t.truthy(output);
+test('set temporary directories', async (t) => {
+  t.truthy(input);
+  t.truthy(output);
 });
 
-test('return a optimized buffer', async t => {
-	const buf = await readFile(path.join(__dirname, 'fixture.gif'));
-	const data = await m({
-		input: buf,
-		bin: gifsicle,
-		args: ['-o', output, input]
-	});
+test('return a optimized buffer', async (t) => {
+  const buf = await readFile(path.join(__dirname, 'fixture.gif'));
+  const data = await m({
+    input: buf,
+    bin: gifsicle,
+    args: ['-o', output, input],
+  });
 
-	t.true(data.length < buf.length);
-	t.true(isGif(data));
+  t.true(data.length < buf.length);
+  t.true(isGif(data));
 });
 
-test('remove temporary files', async t => {
-	// Skip the test on Windows
-	if (process.platform === 'win32') {
-		t.pass();
-		return;
-	}
+test('remove temporary files', async (t) => {
+  // Skip the test on Windows
+  if (process.platform === 'win32') {
+    t.pass();
+    return;
+  }
 
-	const buf = await readFile(path.join(__dirname, 'fixture.gif'));
+  const buf = await readFile(path.join(__dirname, 'fixture.gif'));
 
-	try {
-		await m({
-			input: buf,
-			bin: 'foobarunicorn',
-			args: [output, input]
-		});
-		t.pass();
-	} catch (err) {
-		t.is(err.code, 'ENOENT');
+  try {
+    await m({
+      input: buf,
+      bin: 'foobarunicorn',
+      args: [output, input],
+    });
+    t.pass();
+  } catch (err) {
+    t.is(err.code, 'ENOENT');
 
-		if (err.cause instanceof Error) {
-			t.false(await pathExists(err.cause.spawnargs[0]));
-			t.false(await pathExists(err.cause.spawnargs[1]));
-		}
-	}
+    if (err.cause instanceof Error) {
+      t.false(await pathExists(err.cause.spawnargs[0]));
+      t.false(await pathExists(err.cause.spawnargs[1]));
+    }
+  }
 });
